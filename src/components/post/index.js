ac87c7a219fb42be4297ddb9c1f867cc1daac0cc
Dmitry Zavyalov <zavyalovdmitry@mail.ru>
2021-04-28 19:57:50 +0700

    feat: add actions and footer section to post

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,12 +1,32 @@
 import { useRef } from 'react';
 import PropTypes from 'prop-types';
 import Header from './header';
+import Image from './image';
+import Actions from './actions';
+import Footer from './footer';
+import Comments from './comments';
 
 export default function Post({ content }) {
+  const commentInput = useRef(null);
+  const handleFocus = () => commentInput.current.focus();
   // console.log(content);
   return (
-    <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">
+    <div className="rounded col-span-4 border bg-white border-gray-primary mb-8">
       <Header username={content.username} />
+      <Image src={content.imageSrc} caption={content.caption} />
+      <Actions
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        likedPhoto={content.userLikedPhoto}
+        handleFocus={handleFocus}
+      />
+      <Footer username={content.username} caption={content.caption} />
+      <Comments
+        docId={content.docId}
+        comments={content.comments}
+        posted={content.dateCreated}
+        commentInput={commentInput}
+      />
     </div>
   );
 }
