193ffa210e190c12114a51596ee3bc6c4d14f4a4
Dmitry Zavyalov <zavyalovdmitry@mail.ru>
2022-01-06 15:23:41 -0500

    fix: env vars

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,15 +1,23 @@
+/* eslint-disable prettier/prettier */
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 // import { seedDatabase } from '../seed';
 
+const apiKey = process.env.REACT_APP_API_KEY;
+const authDomain = process.env.REACT_APP_AUTH_DOMAIN;
+const projectId = process.env.REACT_APP_PROJECT_ID;
+const storageBucket = process.env.REACT_APP_STORAGE_BUCKET;
+const messagingSenderId = process.env.REACT_APP_MESSAGING_SENDER_ID;
+const appId = process.env.REACT_APP_APP_ID;
+
 const config = {
-  apiKey: 'AIzaSyDccLnQTJ2C32-s-LrwPPN-VHelGUtqHc8',
-  authDomain: 'instagram-z.firebaseapp.com',
-  projectId: 'instagram-z',
-  storageBucket: 'instagram-z.appspot.com',
-  messagingSenderId: '778901965722',
-  appId: '1:778901965722:web:1471769f4c1e5c56864823'
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 };
 
 const firebase = Firebase.initializeApp(config);
